0ab4d06687ef8c8d37ae4ce54913e8841d56bf53
Shams Tabrej Alam <140603120+the-shams-work@users.noreply.github.com>
2025-05-30 10:34:51 +0000

    minor changes

--- src/routes/content.py
+++ src/routes/content.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from src.utils import GoogleGenerativeAIHandler
-from src.app import app
+from src.app import app, mongo_client
+from src.models import User
 
 router = APIRouter(prefix="/content", tags=["CONTENT"])
 genai = GoogleGenerativeAIHandler()
 
+
 @router.post("/questions")
 async def fetch_questions(request: Request, number_of_questions: int, selected_topics: list[str]):
-    return 
-
+    return genai.generate_questions(number_of_questions=number_of_questions, *selected_topics)
 
 app.include_router(router)
